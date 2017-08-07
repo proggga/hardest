3a4dbd27594e2e25a7ebed7b64d10173a482b88a
Mikhail Fesenko <proggga@gmail.com>
2017-08-07 15:16:47 +0300

    Removed method, fixed first script, group by version

--- tests/test_python_searcher.py
+++ tests/test_python_searcher.py
@@ -17,67 +17,32 @@ import unittest
 class PythonSearcherTestCase(unittest.TestCase):
     """Test case for it."""
 
-    def test_popen(self):
-        """Test."""
-        import subprocess
+    @staticmethod
+    def valid_path(some_file):
         import os
-        my_env = os.environ
-        my_env["PYTHONUNBUFFERED"] = "True"
-        import pty
-        master_fd, slave_fd = pty.openpty()
+        return ((not some_file.endswith('m') and len(some_file) > 5)
+                and not some_file.endswith('-config')
+                and os.path.isfile(some_file)
+                and not os.path.islink(some_file))
 
-        proc = subprocess.Popen(['python'],
-                                # preexec_fn=os.setsid,
-                                stdin=slave_fd,
-                                stdout=subprocess.PIPE,
-                                stderr=subprocess.PIPE,
-                                # executable='/usr/bin/python',
-                                # env=my_env,
-                                # timeout=True,
-                                # universal_newlines=True,
-                                # shell=True)
-                                shell=False)
-        pin = os.fdopen(master_fd, 'w')
-        pin.write('exit(42)\n')
-        # print(proc.stdout.read())
-        # print(proc.stdout.read())
-        # print(proc.stdout.read())
-        # help(proc)
-        import time
-        # print(proc.stdout.readline())
-        # time.sleep(1)
-        # print(proc.stderr.read())
-        # slave_fd.write('exit(1)')
-        stdout, stderr = proc.communicate()
-        print(proc.pid, proc.returncode)
-        # time.sleep(10)
-        print(stdout, stderr)
-        print(stderr)
-
-
-
-    @unittest.skip('skipped this case')
     def test_whereis(self):
         import subprocess
         import os
         import itertools
+        import re
         import collections
         python_list = [
             'python',
             'ironpython',
             'anaconda',
+            'miniconda',
             'jython',
             'micropython',
-            'miniconda',
             'pypy',
             'pyston',
             'stackless',
         ]
-        def valid_path(some_file):
-            return (not some_file.endswith('m')
-                    and not some_file.endswith('-config')
-                    and os.path.isfile(some_file)
-                    and not os.path.islink(some_file))
+
         collected = collections.OrderedDict()
         for python_version in python_list:
             # python_version = 'pypy'
@@ -87,31 +52,34 @@ class PythonSearcherTestCase(unittest.TestCase):
 
             new_list = [found_path for found_path
                         in found_python_versions.strip().split(' ')
-                        if valid_path(found_path)]
+                        if self.valid_path(found_path)]
             if not new_list:
                 continue
             for ver in new_list:
                 try:
-
-                    raw_result = subprocess.check_output([ver, '-c', '"import os; print(os.__file__)"'], stderr=subprocess.STDOUT)
+                    raw_result = subprocess.check_output([ver, '-V'], stderr=subprocess.STDOUT)
                     raw_result = raw_result.decode()
+                    if not raw_result:
+                        continue
                     print(raw_result)
                 except subprocess.CalledProcessError:
                     continue
-                py_version = python_version
-                if raw_result:
-                    if ver not in raw_result.lower():
-                        py_version = raw_result.split(' ')[0]
-                    version = ' '.join(raw_result.splitlines()[0].split(' ')[0:2])
+
+                py_version = str(python_version)
+                if ver not in raw_result.lower():
+                    py_version = raw_result.split(' ')[0]
+                # version = raw_result
+                raw_result = raw_result.replace('\n', ' ')[:-1]
+                raw_result = re.sub(r'\(.*\) ', '', raw_result)
+                try:
+                    internal_dict = collected[raw_result]
                     try:
-                        internal_dict = collected[py_version]
-                        try:
-                            internal_dict[version].append(ver)
-                        except KeyError:
-                            internal_dict[version] = [ver]
+                        internal_dict[os.path.basename(ver)].append(ver)
                     except KeyError:
-                        collected[py_version] = collections.OrderedDict()
-                        collected[py_version][version] = [ver]
+                        internal_dict[os.path.basename(ver)] = [ver]
+                except KeyError:
+                    collected[raw_result] = collections.OrderedDict()
+                    collected[raw_result][os.path.basename(ver)] = [ver]
         import json
         print(json.dumps(collected, indent=4))
         self.assertEqual('qweqw', '')
