cb3dc53eb56b1a54916399dc99550bd251a97612
Mikhail Fesenko <proggga@gmail.com>
2017-08-11 22:44:05 +0300

    Add architecture to module docstring

--- hardest/__init__.py
+++ hardest/__init__.py
@@ -1,4 +1,23 @@
-"""Hardcore test tool module."""
+"""
+Hardcore test tool module.
+
+Architecture:
+
++----------+         +-----------+
+|   CLI    | ----- > | Templator |
++----------+         +-----------+
+      |                     |
+     \|/                   \|/
++-----------------+   +----------+
+| python_searcher |   | Template |
++-----------------+   +----------+       
+         |
+        \|/
+    +--------+
+    | Binary |
+    +--------+
+
+"""
 from hardest.template import Template
 from hardest.templator import Templator
 
