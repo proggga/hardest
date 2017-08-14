79f9df6286ab8b4e6c9d28b5198794032d5ded5a
Mikhail Fesenko <proggga@gmail.com>
2017-08-14 11:56:28 +0300

    Add some logic to scheme.

--- hardest/__init__.py
+++ hardest/__init__.py
@@ -1,21 +1,50 @@
-"""
+r"""
 Hardcore test tool module.
 
-Architecture:
+Class Architecture Scheme:
 
 +----------+         +-----------+
 |   CLI    | ----- > | Templator |
 +----------+         +-----------+
-      |                     |
-     \|/                   \|/
-+-----------------+   +----------+
-| python_searcher |   | Template |
-+-----------------+   +----------+       
-         |
-        \|/
-    +--------+
-    | Binary |
-    +--------+
+ |     |                   |
+ |    \|/                 \|/
+ |  +-------------+   +----------+
+ |  | ConfigSaver |   | Template |
+ |  +-------------+   +----------+
+ |
+ |      +-----------------+
+ +----> | python_searcher |
+        +-----------------+
+                |
+               \|/
+            +--------+
+            | Binary |
+            +--------+
+
+
+||    CLI    ||     Templator     ||   Template   ||
+||  run -----||--->get_template() ||              ||
+||  |        ||     +-------------||-> new()      ||
+||  |        ||     |<------------||---+          ||
+||  |<-------||---<-+ret template ||   |          ||
+||  |        ||                   ||   |          ||
+||  ---- execution of part 1 of algorithm ------  ||
+||  |<-------||-------------------||--+render(str)||
+||  ---- execution of part 2 of algorithm ------  ||
+
+
+||  -------- part 1 of algorithm -----------  ||
+||    CLI    || python_searcher||   Binary    ||
+||  |--------||-->search()     ||             ||
+||  |        ||  |-------------||->new()      ||
+||  |        ||  |<------------||<+-version() ||
+||  |<-------||<-+ret List[str]||             ||
+
+
+||  ---part 2 of algorithm---  ||
+||  CLI      ||  ConfigSaver   ||
+||  |--------||->save(content) ||
+
 
 """
 from hardest.template import Template
