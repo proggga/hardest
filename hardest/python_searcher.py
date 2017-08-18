386a7eb1312a3554ac5ee649033473bf0ba26fec
Mikhail Fesenko <proggga@gmail.com>
2017-08-18 13:20:40 +0300

    Add more debug

--- hardest/python_searcher.py
+++ hardest/python_searcher.py
@@ -97,11 +97,9 @@ class PythonSearcher(object):
         cropped_output = decoded_output.replace(front_unattended_str, '')
         output = cropped_output.strip()
         files_set = set()  # type: Set[str]
-        if not output:
-            return files_set
-        print('files_set', files_set)
-        files_set |= set(output.split(' '))
         files_set.add(sys.executable)
+        if output:
+            files_set |= set(output.split(' '))
         valid_paths = set(filepath for filepath in files_set
                           if self.validator.validate(filepath))
         print('valid_paths1', version_to_search, valid_paths)
